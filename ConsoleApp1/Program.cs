6b3f0bc32958267d68fa91cd5b23a2f002222e6a
Eugen Mamin <jk.mamin@gmail.com>
2020-03-21 17:18:09 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            Triangle[] conclusion = new Triangle[N];
+            double averagesquare = 0;
+            for (int i = 0; i < N; i++)
+            {
+                conclusion[i] = new Triangle();
+                conclusion[i].High();
+                conclusion[i].Perimeter();
+                conclusion[i].Square();
+                Console.WriteLine("-------------------------------------");
+                averagesquare += conclusion[i].square;
+            }
+            averagesquare /= N;
+            Console.WriteLine($"Average Square = {averagesquare}");
+            for (int i = 0; i < N; i++)
+            {
+                if (conclusion[i].square > averagesquare)
+                    Console.WriteLine($"Bigger square then Average Square = {conclusion[i].square}");
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Triangle
+    {
+        double length, high, perimeter;
+        public double square;
+        Random r = new Random();
+        public Triangle()
+        {
+            length = r.Next(0, 20);
+        }
+        public void High()
+        {
+            high = (length * Math.Sqrt(3)) / 2;
+            Console.WriteLine($"High and bisector = {high}");
+        }
+        public void Perimeter()
+        {
+            perimeter = length * 3;
+            Console.WriteLine($"Perimeter = {perimeter}");
+        }
+        public void Square()
+        {
+            square = (length * high) / 2;
+            Console.WriteLine($"Square = {square}");
+        }
+
+    }
+}
